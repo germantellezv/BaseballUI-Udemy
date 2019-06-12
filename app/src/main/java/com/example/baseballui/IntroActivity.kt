d79a893c8c79bf08b5effeb4516616cd95872e0f
German <g.tellezv@hotmail.com>
2019-06-12 07:13:40 -0500

    Slider terminado. Falta configurar el boton "Saltar"

--- app/src/main/java/com/example/baseballui/IntroActivity.kt
+++ app/src/main/java/com/example/baseballui/IntroActivity.kt
@@ -1,12 +1,23 @@
 package com.example.baseballui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_intro.*
 
 class IntroActivity : AppCompatActivity() {
+
+    private lateinit var sliderAdapter: SliderAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intro)
+        sliderAdapter = SliderAdapter(this)
+        introActivityViewPager.adapter = sliderAdapter
+
+        introActivityBtSaltar.setOnClickListener({
+            startActivity(Intent(this@IntroActivity,SplashActivity::class.java))
+        })
+
     }
 }
--- app/src/main/java/com/example/baseballui/SliderAdapter.kt
+++ app/src/main/java/com/example/baseballui/SliderAdapter.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.RelativeLayout
 import androidx.viewpager.widget.PagerAdapter
+import kotlinx.android.synthetic.main.slide_layout.view.*
 
 class SliderAdapter (var context:Context):PagerAdapter() {
 
@@ -29,7 +30,32 @@ class SliderAdapter (var context:Context):PagerAdapter() {
     }
 
     override fun instantiateItem(container: ViewGroup, position: Int): Any {
-        val view = layoutInflater.inflate(R.layout.sli)
+        val view = layoutInflater.inflate(R.layout.slide_layout,container,false)
+        view.slideLayoutRelativeLayout.setBackgroundResource(slide_background[position])
+        view.slideLayoutImageView.setImageResource(slide_images[position])
+        view.slideLayoutTv1.setText(slide_headings[position])
+        view.slideLayoutTv2.setText(slide_descriptions[position])
+        if (position == 0) {
+            view.slideLayoutTvPoint1.setTextColor(context.resources.getColor(R.color.colorWhite))
+            view.slideLayoutTvPoint2.setTextColor(context.resources.getColor(R.color.colorTransparentWhite))
+            view.slideLayoutTvPoint3.setTextColor(context.resources.getColor(R.color.colorTransparentWhite))
+        }
+        if (position == 1) {
+            view.slideLayoutTvPoint1.setTextColor(context.resources.getColor(R.color.colorTransparentWhite))
+            view.slideLayoutTvPoint2.setTextColor(context.resources.getColor(R.color.colorWhite))
+            view.slideLayoutTvPoint3.setTextColor(context.resources.getColor(R.color.colorTransparentWhite))
+        }
+        if (position == 2) {
+            view.slideLayoutTvPoint1.setTextColor(context.resources.getColor(R.color.colorTransparentWhite))
+            view.slideLayoutTvPoint2.setTextColor(context.resources.getColor(R.color.colorTransparentWhite))
+            view.slideLayoutTvPoint3.setTextColor(context.resources.getColor(R.color.colorWhite))
+        }
+        container.addView(view)
+        return view
+    }
+
+    override fun destroyItem(container: ViewGroup, position: Int, `object`: Any) {
+        container.removeView(`object` as (RelativeLayout))
     }
 
 }
